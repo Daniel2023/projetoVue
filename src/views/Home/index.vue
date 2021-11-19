2dde35062d40bb3b6bb30f4fa1d1c384ea6f81bb
Daniel <55217179+Daniel2023@users.noreply.github.com>
2021-11-19 10:17:13 -0300

    first commit

---
+++ src/views/Home/index.vue
@@ -0,0 +1,39 @@
+<template>
+ <custom-header
+  @create-account="handleAccountCreate"
+  @login="handleLogin"
+ />
+
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
+export default {
+  components: { CustomHeader, Contact },
+  setup() {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if(token){
+        router.push({ name:'Feedbacks' })
+      }
+    })
+
+    function handleLogin() {
+    }
+
+    function handleAccountCreate(){
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>
